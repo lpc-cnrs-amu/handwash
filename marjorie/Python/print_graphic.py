17c85523be9024e9a859858d352868a48d7a736e
beignet <rukiadu13@hotmail.fr>
2019-05-23 15:31:04 +0200

    remove texts 0% in pie charts

--- marjorie/Python/print_graphic.py
+++ marjorie/Python/print_graphic.py
@@ -63,6 +63,12 @@ df = pd.DataFrame( {'in': column_in,\
     index = ['puce '+p for p in puce_list])
     
 
+def make_autopct(values):
+    def my_autopct(pct):
+        if pct != 0:
+            return '{p:.2f}%'.format(p=pct)
+    return my_autopct
+
 colors = ['yellowgreen', 'lightcoral']
 labels = ['SHA taken', 'SHA not taken']
 pdf = matplotlib.backends.backend_pdf.PdfPages("SHA_pie.pdf")
@@ -74,7 +80,7 @@ for ind in df.index:
     
     if df.loc[ind,'in'] != -1 and df.loc[ind,'not in'] != -1:   
         ax1 = plt.subplot2grid((1,3),(0,0))
-        df.loc[ind, ['in', 'not in']].plot(kind='pie', ax=ax1, autopct='%1.2f%%', colors=colors, labels=None)
+        df.loc[ind, ['in', 'not in']].plot(kind='pie', ax=ax1, autopct=make_autopct(df.loc[ind, ['in', 'not in']]), colors=colors, labels=None)
         ax1.axis('equal')
         ax1.legend(labels=labels, loc='center left', bbox_to_anchor=(0.5, 0., 0.5, 0.5))
         ax1.set_title('in', pad=0.1)
@@ -82,10 +88,14 @@ for ind in df.index:
     
     if df.loc[ind,'out'] != -1 and df.loc[ind,'not out'] != -1:
         ax2 = plt.subplot2grid((1,3),(0,1))
+        
+        # if the num of the puce and the legends have already been printed
+        # we don't print them again
         if print_in:
-            ax2.pie(df.loc[ind, ['out', 'not out']], autopct='%1.2f%%', colors=colors, labels=None)
+            ax2.pie(df.loc[ind, ['out', 'not out']], autopct=make_autopct(df.loc[ind, ['out', 'not out']]), colors=colors, labels=None)
+        # the num of the puce and the legends have not been printed (only inout activities)
         else:
-            df.loc[ind, ['out', 'not out']].plot(kind='pie', ax=ax2, autopct='%1.2f%%', colors=colors, labels=None)
+            df.loc[ind, ['out', 'not out']].plot(kind='pie', ax=ax2, autopct=make_autopct(df.loc[ind, ['out', 'not out']]), colors=colors, labels=None)
             ax2.legend(labels=labels, loc='center left', bbox_to_anchor=(0.5, 0., 0.5, 0.5))
         ax2.axis('equal')
         ax2.set_title('out', pad=0.1)
@@ -93,10 +103,15 @@ for ind in df.index:
     
     if df.loc[ind,'inout'] != -1 and df.loc[ind,'not inout'] != -1:
         ax3 = plt.subplot2grid((1,3),(0,2))
+        
+        # if the num of the puce and the legends have already been printed
+        # we don't print them again
         if print_in or print_out:
-            ax3.pie(df.loc[ind, ['inout', 'not inout']], autopct='%1.2f%%', colors=colors, labels=None)
+            ax3.pie(df.loc[ind, ['inout', 'not inout']], autopct=make_autopct(df.loc[ind, ['inout', 'not inout']]), colors=colors, labels=None)
+            
+        # the num of the puce and the legends have not been printed (only inout activities)
         else:
-            df.loc[ind, ['inout', 'not inout']].plot(kind='pie', ax=ax3, autopct='%1.2f%%', colors=colors, labels=None)
+            df.loc[ind, ['inout', 'not inout']].plot(kind='pie', ax=ax3, autopct=make_autopct(df.loc[ind, ['inout', 'not inout']]), colors=colors, labels=None)
             ax3.legend(labels=labels, loc='center left', bbox_to_anchor=(0.5, 0., 0.5, 0.5))
         ax3.axis('equal')
         ax3.set_title('inout', pad=0.1)     
