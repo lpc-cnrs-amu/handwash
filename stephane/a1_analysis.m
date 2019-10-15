9298377112888566fca38a5d5ff1911849ffca9b
Stephane Dufau <stephane.dufau@univ-amu.fr>
2019-10-15 19:08:30 +0200

    analysis of events (1)

--- stephane/a1_analysis.m
+++ stephane/a1_analysis.m
@@ -8,7 +8,43 @@ clc
 feature('DefaultCharacterSet','UTF-8');
 
 
-%% Read table
+%% Read tables
+
+%%%%%%%%%%
+% raw data 
+%%%%%%%%%%
+
+filename = "MHT_2018.csv"
+fullfilename = ".." + filesep + "rawdata" + filesep + "Files" + filesep + filename;
+
+opts = delimitedTextImportOptions("NumVariables", 11);
+
+% Specify range and delimiter
+opts.DataLines = [2, Inf];
+opts.Delimiter = ";";
+
+% Specify column names and types
+opts.VariableNames = ["ID", "numTag", "TypeEvenementId", "temps", "AppareilId", "NumAppareil", "CodeEvenement", "NumSHA", "NumAntenne", "Personnel_ID", "tempsImport"];
+opts.VariableTypes = ["double", "double", "double", "datetime", "double", "double", "double", "double", "double", "double", "datetime"];
+
+% Specify file level properties
+opts.ExtraColumnsRule = "ignore";
+opts.EmptyLineRule = "read";
+
+% Specify variable properties
+opts = setvaropts(opts, "temps", "InputFormat", "yyyy-MM-dd HH:mm:ss.SSS");
+opts = setvaropts(opts, "tempsImport", "InputFormat", "yyyy-MM-dd HH:mm:ss.SSS");
+
+opts.Encoding = "UTF-8";
+
+% Import the data
+MHT2018 = readtable("/Users/stephane/Documents/GITHUB/handwash/rawdata/MHT_2018.csv", opts);
+clear opts
+
+
+%%%%%%%%%%%%%%
+% activity csv
+%%%%%%%%%%%%%%
 
 filename = "activities_2018.csv"
 fullfilename = ".." + filesep + "marjorie" + filesep + "Files" + filesep + filename;
@@ -38,7 +74,196 @@ opts.Encoding = "UTF-8";
 activities2018 = readtable(fullfilename, opts);
 clear opts
 
-%%
+%% FIGURES FOR METHOD
+clc
+close all
 
-T = activities2018;
+% figure nb event / nb activity by room
+t1 = tabulate(MHT2018.NumAppareil);
+t2 = tabulate(activities2018.roomID);
+
+ind_in = t1(:,2) | t2(:,2);
+
+figure;
+subplot(1,2,1);
+bar([t1(ind_in,2), t2(ind_in,2)]);
+xlabel('Room');
+ylabel('Count');
+title('Nb evenements 2018');
+legend({'MHT'; 'Activity'})
+
+subplot(1,2,2);
+bar([t1(ind_in,3), t2(ind_in,3)]);
+xlabel('Room');
+ylabel('Percent');
+title('% evenements 2018')
+legend({'MHT'; 'Activity'})
+
+
+
+% figure nb event / nb activity by person
+
+x = MHT2018.numTag;
+x(x < 9 | x > 1000) = [];
+xtab = tabulate(x);
+
+y = activities2018.puceID;
+y(y < 9 | y > 1000) = [];
+ytab = tabulate(y);
+ind_in = xtab(:,2) | ytab(:,2);
+
+[R,P,RL,RU] = corrcoef(xtab(ind_in,3), ytab(ind_in,3))
+
+figure;
+subplot(1,2,1);
+plot(xtab(ind_in,2), ytab(ind_in,2), 'r*');
+xlabel('Nb event');
+ylabel('Nb activity');
+title('Correspondance per person');
+
+subplot(1,2,2);
+plot(xtab(ind_in,3), ytab(ind_in,3), 'b*');
+hold on;
+plot([min(xtab(ind_in,3)) max(xtab(ind_in,3))], [min(ytab(ind_in,3)) max(ytab(ind_in,3))])
+xlabel('% event');
+ylabel('% activity');
+title(['Correspondance per person, R=' num2str(R(1,2), 4)]);
+
+
+% figure nb of event per person
+caty = categorical(y);
+figure;
+histogram(caty);
+title(['Nb of activity per person']);
+ylabel('Count');
+
+ind_in_person = find(ytab(:,2) > 200); % 200 activity IN & OUT
+ID200 = ytab(ind_in_person,1);
+caty = categorical(y(ismember(y, ID200)));
+figure;
+histogram(caty);
+title(['Nb of activity per person >200 (N=58)']);
+ylabel('Count');
+
+
+
+% question: y'a t il des comportement qui ne sont pas conanoique et sont
+% plus exclu que d'autre? --> representativité de l'echantillon
+% analyse des abandons par personne, par piece dans activites
+
+% pourcentage d'abandon par personne
+t = activities2018(:,{'puceID', 'abort','statut'});
+
+t = t(~contains(string(t.statut), 'inout'),:); % take only IN and OUT (not INOUT)
+t = t(ismember(t.puceID, ID200), :); % take only ID with more than 200 activity
+t.valid = double(t.abort == "valide");
+
+valid_per_person = grpstats(t, {'puceID'}, 'mean', 'DataVars',{'valid'})
+
+figure;
+subplot(1,2,1);
+boxplot(valid_per_person.mean_valid);
+title('Percent of valid activities, N=58');
+
+subplot(1,2,2);
+plot(valid_per_person.GroupCount, valid_per_person.mean_valid, 'r*');
+xlabel('Nb activity');
+ylabel('Percent valid');
+title('Valid activities');
+
+
+
+% pourcentage d'abandon par piece
+tr = activities2018(:,{'roomID', 'abort','statut'});
+
+yr = activities2018.roomID;
+yr(yr > 20) = [];
+yrtab = tabulate(yr);
+
+
+tr = tr(~contains(string(tr.statut), 'inout'),:); % take only IN and OUT (not INOUT)
+tr = tr(tr.roomID<=20, :); % take only roomID with more than 200 activity
+tr.valid = double(tr.abort == "valide");
+
+valid_per_room = grpstats(tr, {'roomID'}, 'mean', 'DataVars',{'valid'})
+
+figure;
+subplot(1,2,1);
+boxplot(valid_per_room.mean_valid);
+title('Percent of valid activities, N=20 (room)');
+
+subplot(1,2,2);
+plot(valid_per_room.GroupCount, valid_per_room.mean_valid, 'r*');
+xlabel('Nb activity');
+ylabel('Percent valid');
+title('Valid activities');
+
+
+
+% question: un out est-il vraiment un out?
+% --> pourcentage d'activité taguée IN et OUT, mais entrecoupé d'une
+% présence dans une autre pièce.
+% deja codé dans raison d'abandon (activite entremelee)
+t.entrem = double(t.abort == "activite entremelee");
+entrem_per_person = grpstats(t, {'puceID'}, 'mean', 'DataVars',{'entrem'})
+
+figure;
+subplot(1,2,1);
+boxplot(entrem_per_person.mean_entrem);
+title('Percent of interlock activities, N=58');
+
+subplot(1,2,2);
+plot(entrem_per_person.GroupCount, entrem_per_person.mean_entrem, 'r*');
+xlabel('Nb activity');
+ylabel('Percent interlock');
+title('Interlock activities');
+
+
+
+% histogramme des durée + regarder les gigantesque a quoi elles
+% correspondent
+
+d = activities2018.duration;
+% TODO : make the total durtion of IN + OUT
+
+% verifier les 2 types de soins (cour: infirmier), long aide-soignat:
+% analyse des durée par tag
+
+
+
+
+
+
+%% FIGURES FOR SCIENCE
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
